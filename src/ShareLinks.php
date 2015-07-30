c9165dfb2c0bd6b7bd0aaa30015499fcd41d9eed
Dan Hughes <danhughes.dh+github@gmail.com>
2015-07-30 20:45:14 +0000

    Use PSR2 formatting

--- src/ShareLinks.php
+++ src/ShareLinks.php
@@ -1,23 +1,25 @@
 <?php namespace xes;
-class ShareLinks {
-	private static $links = [
-		'facebook' => 'http://facebook.com/sharer/sharer.php?u={{link}}',
-		'twitter' => 'http://twitter.com/home?status={{link}}',
-		'google-plus' => 'http://plus.google.com/share?url={{link}}',
-		'pinterest' => 'http://pinterest.com/pin/create/button/?url={{link}}&media={{image}}'
-	];
 
-	public static function URL($network, $link, $image = null) {
-		if (!array_key_exists($network, self::$links)) {
-			throw new \Exception("$network is not supported");
-		}
-		$template = self::$links[$network];
+class ShareLinks
+{
+    private static $links = [
+    'facebook' => 'http://facebook.com/sharer/sharer.php?u={{link}}',
+    'twitter' => 'http://twitter.com/home?status={{link}}',
+    'google-plus' => 'http://plus.google.com/share?url={{link}}',
+    'pinterest' => 'http://pinterest.com/pin/create/button/?url={{link}}&media={{image}}'
+    ];
 
-		if ($image) {
-			$template = str_replace('{{image}}', urlencode($image), $template);
-		}
+    public static function URL($network, $link, $image = null)
+    {
+        if (!array_key_exists($network, self::$links)) {
+            throw new \Exception("$network is not supported");
+        }
+        $template = self::$links[$network];
 
-		return str_replace('{{link}}', urlencode($link), $template);
-	}
+        if ($image) {
+            $template = str_replace('{{image}}', urlencode($image), $template);
+        }
+
+        return str_replace('{{link}}', urlencode($link), $template);
+    }
 }
-?>
